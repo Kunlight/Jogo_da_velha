acd8b6e0a495951808cd29ed1957873454b3ba49
Kunlight <atilakiller51@gmail.com>
2018-04-11 21:42:37 -0300

    Versão final sem vetores e sem perfumaria

--- tictactoe.c
+++ tictactoe.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
         
 	int jogo(char *A1, char *B1, char *C1, char *A2, char *B2, char  *C2, char *A3, char *B3, char *C3, int *jogador){
-		printf( "		   A          B        C			\n");		
+                printf( "		   A          B        C			\n");		
 		printf( " 			#           #	  \n");	
 		printf( " 		  	#           #	  \n");	
 		printf( " 1                  %c   #     %c     #   %c  \n", *A1, *B1, *C1);		
@@ -20,14 +20,18 @@
 		printf( "			#           #		\n");
         printf( "                    %c   #     %c     #   %c  \n", *A3, *B3, *C3);	
 		printf( " 3			#           #		\n");	
-		printf( " 			#           #		\n");	
-
+		printf( " 			#           #		\n");
+                
                 if((*A1 != ' ' && *A1 == *B1 && *B1 == *C1) || (*A1 != ' ' && *A1 == *A2 && *A2 == *A3) || (*A1 != ' ' && *A1 == *B2 && *B2 == *C3) || (*A3 != ' ' && *A3 == *B2 && *B2 == *C1) || (*B1 != ' ' && *B1 == *B2 && *B2 == *B3) || (*A2 != ' ' && *A2 == *B2 && *B2 == *C2) || (*A3 != ' ' && *A3 == *B3 && *B3 == *C3) || (*C1 != ' ' && *C1 == *C2 && *C2 == *C3)){
                 printf( " FIM DE JOGO!!!, JOGADOR %d GANHOU!!! \n", *jogador );
                 exit(0);
+                }
+                else if(*A1 != ' ' && *A2 != ' ' && *A3 != ' ' && *B1 != ' ' && *B2 != ' ' && *B3 != ' ' && *C1 != ' ' && *C2 != ' ' && *C3 != ' '){
+           printf("Deu velha!\n");
+                exit(0);       
                 }else{
 		return 0;
-	        }
+	        }      
         }
 
         char escolha_jogador(int *jogador){
@@ -39,14 +43,6 @@
                    return 'O';
                 }
           }    
-
-         /* void teste_escolha(char posicao, char *local){
-                if(strcmp(escolha, posicao) == 0){
-		   *local = escolha_jogador(&jogador);
-                jogo(A1, B1, C1, A2, B2, C2, A3, B3, C3);
-		}
-        } */ 
-	
               int main(){
 		char A1, B1, C1, A2, B2, C2, A3, B3, C3;
                 int jogador;
@@ -61,8 +57,12 @@
 		C3 = ' ';
                 jogador = 1;
 		char escolha[3];
-		printf( "Bem vindo, deseja jogar contra a máquina ou contra outro jogador?\n" );
-		printf( "Esta será a base, decore as casas de A a B e 1 a 3\n"	); 
+                printf( "\n\n\n\n" );
+		printf( "Ｂｅｍ ｖｉｎｄｏ ａｏ ｍｅｕ ｄｏｍíｎｉｏ, ｓｅｒｅｓ ｉｎｆｅｒｉｏｒｅｓ.\n" );
+                printf( "Ｃｒｅｉｏ ｑｕｅ ｄｅｓｅｊａｍ ｂａｔａｌｈａｒ ｅｎｔｒｅ ｓｉ, ｐｏｉｓ ｅｕ ｓｅｉ ｑｕｅ ｅｓｔｅ ｄｉａ ｎãｏ ａｇｕａｒｄａ ｎｏｓｓａ ｂａｔａｌｈａ　因ぅ違.\n");
+                printf( "Ｕｍ ｍｅｒｏ ｃｏｍｐｕｔａｄｏｒ é ｏ ｓｕｆｉｃｉｅｎｔｅ ｐａｒａ ｇｕｉá-ｌｏｓ, ｅｓｔａｒｅｉ ｏｂｓｅｒｖａｎｄｏ.\n\n" );
+		printf( "Bem vindos ao jogo da velha, boa sorte a ambos!\n" );
+                printf( "Esta será a base, decore as casas de A a B e 1 a 3\n"	); 
 		printf( "%c", jogo(&A1, &B1, &C1, &A2, &B2, &C2, &A3, &B3, &C3, &jogador));		
 		printf( " Ótimo, vamos começar! Começando pelo jogador 1, ele também será o X. \n" );
 		printf( " Você deve dizer a posição que deseja jogar usando os números e as letras, exemplo: B1, C2, A3. \n" );
@@ -72,49 +72,83 @@
                 scanf("%s", escolha);
                 
                 
-if(strcmp(escolha, "A1") == 0){
-                printf( "Escolheu A1.\n" );
-		   A1 = escolha_jogador(&jogador);
-                jogo(&A1, &B1, &C1, &A2, &B2, &C2, &A3, &B3, &C3, &jogador);
-		}
-		else if(strcmp(escolha, "B1") == 0){
-		   B1 = escolha_jogador(&jogador);
-                jogo(&A1, &B1, &C1, &A2, &B2, &C2, &A3, &B3, &C3, &jogador);
-		}
-		else if(strcmp(escolha, "C1") == 0){
-		   C1 = escolha_jogador(&jogador);
-                jogo(&A1, &B1, &C1, &A2, &B2, &C2, &A3, &B3, &C3, &jogador);
-		}
-		else if(strcmp(escolha, "A2") == 0){
-		   A2 = escolha_jogador(&jogador);
-                jogo(&A1, &B1, &C1, &A2, &B2, &C2, &A3, &B3, &C3, &jogador);
-		}
-		else if(strcmp(escolha, "B2") == 0){
-		   B2 = escolha_jogador(&jogador);
-                jogo(&A1, &B1, &C1, &A2, &B2, &C2, &A3, &B3, &C3, &jogador);
-		}
-		else if(strcmp(escolha, "C2") == 0){
-		   C2 = escolha_jogador(&jogador);
-                jogo(&A1, &B1, &C1, &A2, &B2, &C2, &A3, &B3, &C3, &jogador);
-		}
-		else if(strcmp(escolha, "A3") == 0){
-		   A3 = escolha_jogador(&jogador);
-                jogo(&A1, &B1, &C1, &A2, &B2, &C2, &A3, &B3, &C3, &jogador);
-		}
-		else if(strcmp(escolha, "B3") == 0){
-		   B3 = escolha_jogador(&jogador);
-                jogo(&A1, &B1, &C1, &A2, &B2, &C2, &A3, &B3, &C3, &jogador);
-		}
-		else if(strcmp(escolha, "C3") == 0){
-		   C3 = escolha_jogador(&jogador);
-                jogo(&A1, &B1, &C1, &A2, &B2, &C2, &A3, &B3, &C3, &jogador);
+                if(strcmp(escolha, "A1") == 0){
+                   if(A1 == ' '){
+		        A1 = escolha_jogador(&jogador);
+                        printf("\e[H\e[2J"); //Dará um clear na tela, pegue no site https://www.vivaolinux.com.br/dica/Como-limpar-a-tela-em-C
+                        jogo(&A1, &B1, &C1, &A2, &B2, &C2, &A3, &B3, &C3, &jogador);
+		   }else{
+                        printf( "Posição já tomada.\n" );
+                   }
+                }else if(strcmp(escolha, "B1") == 0){
+                        if(B1 == ' '){
+		                B1 = escolha_jogador(&jogador);
+                                printf("\e[H\e[2J");
+                                jogo(&A1, &B1, &C1, &A2, &B2, &C2, &A3, &B3, &C3, &jogador);
+		   }else{
+                                printf( "Posição já tomada.\n" );
+                        }
+		}else if(strcmp(escolha, "C1") == 0){
+                        if(C1 == ' '){
+		                C1 = escolha_jogador(&jogador);
+                                printf("\e[H\e[2J");
+                                jogo(&A1, &B1, &C1, &A2, &B2, &C2, &A3, &B3, &C3, &jogador);
+		         }else{
+                                printf( "Posição já tomada.\n" );
+                              }
+		}else if(strcmp(escolha, "A2") == 0){
+                        if(A2 == ' '){
+		                A2 = escolha_jogador(&jogador);
+                                printf("\e[H\e[2J");
+                                jogo(&A1, &B1, &C1, &A2, &B2, &C2, &A3, &B3, &C3, &jogador);
+		         }else{
+                                printf( "Posição já tomada.\n" );
+                              }
+		}else if(strcmp(escolha, "B2") == 0){
+		        if(B2 == ' '){
+                                B2 = escolha_jogador(&jogador);
+                                printf("\e[H\e[2J");
+                                jogo(&A1, &B1, &C1, &A2, &B2, &C2, &A3, &B3, &C3, &jogador);
+		         }else{
+                                printf( "Posição já tomada.\n" );
+                              }
+		}else if(strcmp(escolha, "C2") == 0){
+                        if(C2 == ' '){
+		                C2 = escolha_jogador(&jogador);
+                                printf("\e[H\e[2J");
+                                jogo(&A1, &B1, &C1, &A2, &B2, &C2, &A3, &B3, &C3, &jogador);
+		        }else{
+                                printf( "Posição já tomada.\n" );
+                             }
+		}else if(strcmp(escolha, "A3") == 0){
+                        if(A3 == ' '){
+		                A3 = escolha_jogador(&jogador);
+                                printf("\e[H\e[2J");
+                                jogo(&A1, &B1, &C1, &A2, &B2, &C2, &A3, &B3, &C3, &jogador);
+		        }else{
+                                printf( "Posição já tomada.\n" );
+                             }
+		}else if(strcmp(escolha, "B3") == 0){
+                        if(B3 == ' '){
+		                B3 = escolha_jogador(&jogador);
+                                printf("\e[H\e[2J");
+                                jogo(&A1, &B1, &C1, &A2, &B2, &C2, &A3, &B3, &C3, &jogador);
+                        }else{
+                                printf( "Posição já tomada.\n");
+                             }
+		}else if(strcmp(escolha, "C3") == 0){
+                        if(C3 == ' '){
+		                C3 = escolha_jogador(&jogador);
+                                printf("\e[H\e[2J");   
+                                jogo(&A1, &B1, &C1, &A2, &B2, &C2, &A3, &B3, &C3, &jogador);
+                        }else{
+                                printf( "Posição já tomada.\n" );
+                             }
 		}else if(strcmp(escolha, "S") == 0){
                    exit(0);
                 }else{
 		   printf( "Esta não é uma posição válida. %s\n", escolha);
-		}
-
-
+		}        
               
 	        }	
         return 0;
